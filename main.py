99335f924ca82040b1bce4ed34007d7cb2d4b286
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-14 12:06:20 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from data import Reading_files
+
+# tutaj wszystko wywołujemy
+
+# test wczytywania plików
+path = "./ParlaMint-PL.conllu/ParlaMint-PL_2015-11-12-sejm-01-1.conllu"
+file_reading = Reading_files(path=path)
