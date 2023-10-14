8f8cdcaa5113c93d25e312fbc7db85c1d836c520
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-14 15:35:40 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,6 @@
+import model
+
+
 class Reading_files:
     def __init__(self, path):
         self.path = path
@@ -12,3 +15,17 @@ class Reading_files:
                 if line.startswith("# text"):
                     text_lines.append(line)
         return text_lines
+
+    def combine_text_and_emotion(self):
+        """
+        Wstępnie stworzony słownik = {tekst: emocja} -> może się przydać do wizualizacji
+        """
+        combined_dictionary = {}
+        text_lines = self.read_file()
+
+        for line in text_lines:
+            cleaned_line = line.strip("# text").strip()
+            emotion = model.get_emotion(cleaned_line)
+            combined_dictionary[cleaned_line] = emotion
+
+        return combined_dictionary
