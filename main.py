4f092a078caf6e40cf72b887e921dad01bae02c5
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-15 09:15:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,13 @@ from data import Reading_files
 
 def main():
     print("Please wait...")
+
     # test wczytywania plików
     path = "./ParlaMint-PL.conllu/ParlaMint-PL_2015-11-12-sejm-01-1.conllu"
     file_reading = Reading_files(path=path)
 
-    # text_lines = file_reading.read_file()
-    # for line in text_lines:
-    #     print(line.strip("# text").strip())
-
-    # inicjalizacja modelu
-    # for line in text_lines:
-    #     print(model.get_emotion(line.strip("# text").strip()))
-
     print(file_reading.combine_text_and_emotion())
+
     print("Done!")
 
 
