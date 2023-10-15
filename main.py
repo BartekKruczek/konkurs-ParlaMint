cd6793e183bb57718e6469b198ac9d8dd386c3c5
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-15 10:29:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
     path = "./ParlaMint-PL.conllu/ParlaMint-PL_2015-11-12-sejm-01-1.conllu"
     file_reading = Reading_files(path=path)
 
-    print(file_reading.combine_text_and_emotion())
+    # tworzenie słownika z tekstem i emocją
+    file_reading.combine_text_and_emotion()
+
+    # zbieranie wszystkich słowników w jeden słownik
+    print(file_reading.combine_all_to_one_dictionary())
 
     print("Done!")
 
