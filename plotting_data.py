e3dacc7f4ebb20fbfa263047c750019af26559ab
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-15 10:37:02 +0200

    Create plotting_data.py

---
+++ plotting_data.py
@@ -0,0 +1,6 @@
+class Plotting_data:
+    def __init__(self, master_dictionary) -> None:
+        self.master_dictionary = master_dictionary
+
+    def __repr__(self) -> str:
+        return "Klasa do wizualizacji danych"
