899985668abb402c0dbe6def583d093fc842eeba
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-17 15:48:29 +0200

    Update main.py
    Znaleźć inny sposób wyciągnięcia płci mówcy, zmienić dataset na 3.0

--- main.py
+++ main.py
@@ -13,18 +13,21 @@ def main():
     path = "./ParlaMint-PL.conllu/ParlaMint-PL_2015-11-12-sejm-01-1.conllu"
     file_reading = Reading_files(path=path)
 
+    # testowanie funkcji read_file
+    print(file_reading.read_file())
+
     # tworzenie słownika z tekstem i emocją
     file_reading.combine_text_and_emotion()
 
     # zbieranie wszystkich słowników w jeden słownik
     master_dictionary = file_reading.combine_all_to_one_dictionary()
-    print(master_dictionary)
+    # print(master_dictionary)
 
     # inicjalizacja klasy do wizualizacji danych
     plotting_data = Plotting_data(master_dictionary=master_dictionary)
 
     # tworznie chmury emocji
-    plotting_data.plotting_emotion_cloud()
+    # plotting_data.plotting_emotion_cloud()
 
     print("Done!")
 
