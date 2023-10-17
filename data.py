36918584c220d49d7ae3374a5aa84ca53ffb4121
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-17 15:34:36 +0200

    Update data.py
    Próba ekstrakcji płci mówcy

--- data.py
+++ data.py
@@ -11,39 +11,43 @@ class Reading_files:
 
     def read_file(self):
         text_lines = []
+        gender_info = []
         with open(self.path, "r", encoding="utf-8") as f:
             for line in f:
                 if line.startswith("# text"):
                     text_lines.append(line)
-        return text_lines
+                if line.startswith("# speaker_gender"):
+                    gender_info.append(line.split()[-1])
+        return text_lines, gender_info
 
     def combine_text_and_emotion(self):
         """
         Wstępnie stworzony słownik = {tekst: emocja} -> może się przydać do wizualizacji
         """
-        combined_dictionary = {}
-        text_lines = self.read_file()
+        combined_list = []
+        text_lines, gender_info = self.read_file()
 
-        for line in text_lines:
+        for i, line in enumerate(text_lines):
             cleaned_line = line.strip("# text").strip()
             emotion = model.get_emotion(cleaned_line)
-            combined_dictionary[cleaned_line] = emotion
 
-        # czyszczenie słownika
-        combined_dictionary = {
-            key.lstrip("= ").strip(): value.lstrip("<pad> ").strip()
-            for key, value in combined_dictionary.items()
-        }
+            # Jeśli dostępna jest informacja o płci mówcy, dodaj ją do listy
+            if i < len(gender_info):
+                gender = gender_info[i]
+                combined_list.append([cleaned_line, emotion, gender])
+            else:
+                combined_list.append([cleaned_line, emotion])
 
-        return combined_dictionary
+        return combined_list
 
     def combine_all_to_one_dictionary(self):
         """
-        Słownik master, gdzie kluczem jest nazwa pliku, a wartością słownik z emocjami -> {nazwa_pliku: {tekst: emocja}}
+        Słownik master, gdzie kluczem jest nazwa pliku, a wartością lista zawierająca tekst, emocję i płeć mówcy:
+        {nazwa_pliku: [["tekst", emocja, płeć mówcy], ["tekst", emocja, płeć mówcy], ...]}
         """
         master_dictionary = {}
 
-        # pobieranie nazwy pliku
+        # Pobieranie nazwy pliku
         file_name = os.path.basename(self.path)
 
         master_dictionary[file_name] = self.combine_text_and_emotion()
