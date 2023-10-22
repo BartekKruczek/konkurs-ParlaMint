8a29f78e71c32be86f45c93ac656d72d99b3825c
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-10-22 15:32:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ Jak na razie przetestowane dla jednego pliku. Dodać funkcjonalość dla wielu p
 
 
 def main():
-    print("Please wait...", file=open("logs.txt", "a"))
+    print("Please wait...", file=open("logs.txt", "a", encoding="utf-8"))
 
     # test wczytywania plików
     path = "./ParlaMint-PL.txt"
     file_reading = Reading_files(path=path)
 
-    df = file_reading.read_txt_file()
-    # print(df.head(10), file=open("logs.txt", "a"))
-    print(type(df), file=open("logs.txt", "a"))
-
-    file_reading.write_to_txt(df=df)
+    dataframes = file_reading.read_txt_file()
+    print(dataframes, file=open("logs.txt", "a", encoding="utf-8"))
 
     # tworzenie słownika z tekstem i emocją
     # file_reading.combine_text_and_emotion()
@@ -33,10 +30,10 @@ def main():
     # tworznie chmury emocji
     # plotting_data.plotting_emotion_cloud()
 
-    print("Done!", file=open("logs.txt", "a"))
+    print("Done!", file=open("logs.txt", "a", encoding="utf-8"))
 
     # wysyłanie konsoli do pliku
-    sys.stdout = open("logs.txt", "a")
+    sys.stdout = open("logs.txt", "a", encoding="utf-8")
     sys.stdout.close()
 
 
