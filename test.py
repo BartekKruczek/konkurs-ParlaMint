2cf95b1062b0cfb3b2fa9a7c4b974b0c725c8155
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-11-01 20:26:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -16,3 +16,21 @@ df2 = pd.DataFrame(dic2)
 
 df_list = [df, df2]
 print(df_list)
+
+
+emotions_speech = []
+covid_emotions_speech = []
+
+for wypowiedz in df_list:
+    emotions_speech += list(wypowiedz["emotion"])
+    covid_emotions_speech += [
+        emotion
+        for emotion, subcorpus in zip(
+            list(wypowiedz["emotion"]), list(wypowiedz["Subcorpus"])
+        )
+        if "a" in subcorpus
+    ]
+
+
+print(emotions_speech)
+print(covid_emotions_speech)
