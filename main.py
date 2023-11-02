6cc234ac9cae4eb12e9237b3610113488d1b1a35
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-11-02 21:46:05 +0100

    Update main.py
    Naprawa błędu

--- main.py
+++ main.py
@@ -14,10 +14,33 @@ def main():
     path = "./ParlaMint-PL.txt"
     file_reading = Reading_files(path=path)
 
+    # test
+    cleaned = file_reading.cleaning_text()
+    print(cleaned[0].head(10), file=open("logs.txt", "a", encoding="utf-8"))
+    print(
+        type(cleaned[0]["Subcorpus"][0]), file=open("logs.txt", "a", encoding="utf-8")
+    )
+    emotions_speech = []
+    for wypowiedz in cleaned:
+        emotions_speech += list(wypowiedz["speech_id"])
+    print(emotions_speech[0], file=open("logs.txt", "a", encoding="utf-8"))
+    print(type(emotions_speech[0]), file=open("logs.txt", "a", encoding="utf-8"))
+
+    covid_emotions_speech = []
+    for wypowiedz in cleaned:
+        emotions_speech += list(wypowiedz["speech_id"])
+        covid_emotions_speech += [
+            emotion
+            for emotion, subcorpus in zip(
+                list(wypowiedz["speech_id"]), list(wypowiedz["Subcorpus"])
+            )
+            if "COVID" in subcorpus
+        ]
+
     # rysoawnie wykresu częstości występowania emocji
-    x, y = file_reading.draw_emotion_frequency()
-    for a, b in zip(x, y):
-        print(type(a), type(b), file=open("logs.txt", "a", encoding="utf-8"))
+    # x, y = file_reading.draw_emotion_frequency()
+    # for a, b in zip(x, y):
+    #     print(type(a), type(b), file=open("logs.txt", "a", encoding="utf-8"))
 
     print("Done!", file=open("logs.txt", "a", encoding="utf-8"))
 
