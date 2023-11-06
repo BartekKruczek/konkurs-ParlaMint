c8789fa528f9914c03362cb71e4bca04011673e7
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-11-06 19:41:35 +0100

    Update test_modelu.py

--- test_modelu.py
+++ test_modelu.py
@@ -1,21 +1,29 @@
-from transformers import AutoTokenizer, AutoModelWithLMHead, AutoModelForCausalLM
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from googletrans import Translator
+
+translator = Translator()
+
 
 tokenizer = AutoTokenizer.from_pretrained(
     "glombardo/misogynistic-statements-classification-model"
 )
-
-model = AutoModelForCausalLM.from_pretrained(
-    "glombardo/misogynistic-statements-classification-model", is_decoder=True
+model = AutoModelForSequenceClassification.from_pretrained(
+    "glombardo/misogynistic-statements-classification-model"
 )
 
-text = "Test text"
+# tekst nie ma na celu obrażać kobiet, jest wykorzystywany w celach testowych i naukowych
+text = "Chcę cię zgwałcić wredna suko"
+translated_text = translator.translate(text, src="pl", dest="es").text
+print(translated_text)
 
+input_ids = tokenizer.encode(text, return_tensors="pt")
+output = model(input_ids)
 
-def get_emotion(text):
-    input_ids = tokenizer.encode(text + "</s>", return_tensors="pt")
-
-    output = model.generate(input_ids=input_ids, max_length=2)
-
-    dec = [tokenizer.decode(ids) for ids in output]
-    label = dec[0]
-    return label
+if (
+    output.logits.softmax(dim=1)[0].tolist()[0]
+    > output.logits.softmax(dim=1)[0].tolist()[1]
+):
+    print("Not misogynistic")
+else:
+    print("Misogynistic")
