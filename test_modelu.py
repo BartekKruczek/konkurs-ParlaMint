c55932b940ef266651284003e2e52f6cf72c4673
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-11-07 10:32:23 +0100

    Update test_modelu.py

--- test_modelu.py
+++ test_modelu.py
@@ -1,48 +1,14 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
 from deep_translator import GoogleTranslator
-import pandas as pd
 
-
-tokenizer = AutoTokenizer.from_pretrained(
-    "glombardo/misogynistic-statements-classification-model"
-)
-model = AutoModelForSequenceClassification.from_pretrained(
-    "glombardo/misogynistic-statements-classification-model"
+classifier = pipeline(
+    task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None
 )
 
-# tekst nie ma na celu obrażać kobiet, jest wykorzystywany w celach testowych i naukowych
-text = "Jesteś najwredniejszym i najgłupszym facetem na świecie."
-df = pd.DataFrame()
-df["text"] = [text]
-text2 = "Jesteś najwredniejszą i najgłupszą kobietą na świecie."
-df2 = pd.DataFrame()
-df2["text"] = [text2]
-dfl = []
-dfl.append(df)
-dfl.append(df2)
+sentences = "Serdecznie witam pierwszą osobę w państwie - pana prezydenta Rzeczypospolitej Polskiej. [[Długotrwałe oklaski]] Kłaniam się bardzo nisko i dziękuję za przybycie wszystkim dostojnym gościom. Swoją obecnością uświetniacie państwo tę inaugurację. Wielkie to dla nas uhonorowanie. [[Oklaski]] Pozdrawiam szanownych posłów. Witam słuchających w mediach. [[Oklaski]] Proszę o powstanie i uczczenie chwilą ciszy zmarłych, którzy służyli ojczyźnie. [[Chwila ciszy]] Dziękuję bardzo. Proszę prezydenta Rzeczypospolitej Andrzeja Dudę o wygłoszenie orędzia na 1. posiedzeniu Sejmu VIII kadencji."
+translated_sentences = GoogleTranslator(source="auto", target="en").translate(sentences)
 
-misoginic_df = []
-
-for i in range(0, len(dfl)):
-    df_copy = dfl[i].copy()
-    text_from_df = df_copy["text"].to_string(index=False)
-    # print(text_from_df)
-    # print(type(text_from_df))
-    translated_text = GoogleTranslator(src="auto", target="es").translate(text_from_df)
-
-    input_ids = tokenizer.encode(text_from_df, return_tensors="pt")
-    output = model(input_ids)
-
-    if (
-        output.logits.softmax(dim=1)[0].tolist()[0]
-        > output.logits.softmax(dim=1)[0].tolist()[1]
-    ):
-        df_copy["misoginic"] = "No"
-        misoginic_df.append(df_copy)
-    else:
-        df_copy["misoginic"] = "Yes"
-        misoginic_df.append(df_copy)
-
-for i in range(0, len(misoginic_df)):
-    print(misoginic_df[i]["misoginic"])
+model_outputs = classifier(translated_sentences)
+print(model_outputs[0][0])
+print(model_outputs[0][0]["label"])
+# produces a list of dicts for each of the labels
