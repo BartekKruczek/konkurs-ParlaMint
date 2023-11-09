e2157c2132fe28453b407fb486598f7d9b9da9d2
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-11-09 20:40:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ Jak na razie przetestowane dla jednego pliku. Dodać funkcjonalość dla wielu p
 def main():
     print("Please wait...", file=open("logs.txt", "a", encoding="utf-8"))
 
-    # test wczytywania plików
+    # wczytywania plików
     path = "./ParlaMint-PL.txt"
     file_reading = Reading_files(path=path)
 
+    # test
+    file_reading.getting_emotion_new_model()
+
     # rysoawnie wykresu częstości występowania emocji
-    file_reading.draw_emotion_frequency()
+    # file_reading.draw_emotion_frequency()
 
     print("Done!", file=open("logs.txt", "a", encoding="utf-8"))
 
